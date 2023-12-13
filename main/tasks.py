b5c49680d2733e77c2807ce5a844ce2142f4c55c
ic2dent <shamyratbabanazarow@gmail.com>
2023-12-13 20:20:19 +0500

    Правки

--- main/tasks.py
+++ main/tasks.py
@@ -9,21 +9,25 @@ from .models import PlannedMessage, TgUser
 def send_planned_messages(bot: TeleBot):
     messages = PlannedMessage.objects.filter(send_time__lte=timezone.now(), is_sent=False)
     for message in messages:
-        message_text = message.text.replace('<p>', '').replace('</p>', '').replace('<br />', '')
-        message_text = html.unescape(message_text)
+        try:
 
-        additional_text = None
-        if message.additional_info is not None and message.additional_info != "":
-            additional_text = message.additional_info.replace('<p>', '').replace('</p>', '').replace('<br />', '')
-            additional_text = html.unescape(additional_text)
+            message_text = message.text.replace('<p>', '').replace('</p>', '').replace('<br />', '')
+            message_text = html.unescape(message_text)
 
-        photo = open(message.message_image.path, 'rb')
-        for user in TgUser.objects.all():
-            bot.send_photo(user.user_id, photo, caption=message_text, parse_mode='HTML')
+            additional_text = None
+            if message.additional_info is not None and message.additional_info != "":
+                additional_text = message.additional_info.replace('<p>', '').replace('</p>', '').replace('<br />', '')
+                additional_text = html.unescape(additional_text)
 
-            if additional_text is not None:
-                bot.send_message(user.user_id, additional_text, parse_mode='HTML')
-        photo.close()
+            photo = open(message.message_image.path, 'rb')
+            for user in TgUser.objects.all():
+                bot.send_photo(user.user_id, photo, caption=message_text, parse_mode='HTML')
 
-        message.is_sent = True
-        message.save()
+                if additional_text is not None:
+                    bot.send_message(user.user_id, additional_text, parse_mode='HTML')
+            photo.close()
+
+            message.is_sent = True
+            message.save()
+        except Exception as e:
+            print(f"Error sending message {message.id}: {str(e)}")
